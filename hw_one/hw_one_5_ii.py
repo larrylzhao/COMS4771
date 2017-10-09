dccc5caa83d0f8f6a75c559baf2b5e35580ef24e
larryzhao <larrylzhao@gmail.com>
2017-10-08 20:14:30 -0500

    5ii done

--- hw_one/hw_one_5_ii.py
+++ hw_one/hw_one_5_ii.py
@@ -1,8 +1,6 @@
-# Essentially calculate the k closest distances
-# do this by subtracting training point array from test point array
-# square the differences and add them all up
-# then take the square root of that sum
-
+# with a 70/30 training/test split, k=1, and norm L2:
+# 2841 classified correctly, and 159 incorrectly,
+# leading to an accuracy of 94.7%
 
 import scipy.io
 import matplotlib.pyplot as plt
@@ -14,19 +12,12 @@ import operator
 np.set_printoptions(threshold='nan')
 mat = scipy.io.loadmat('hw1data.mat')
 
-def distance(x, y):
-    d = 0
-    for i in range(len(x)):
-        d += pow((x[i] - y[i]), 2)
-    d = math.sqrt(d)
-    return d
-
 def knn(k, training, test):
     distances = [None]*len(training)
 
     for i in range(len(training)):
-        d = distance(training[i], test)
-        distances[i] = (d, training[i])
+        d = np.linalg.norm(training[i] - test, ord=2)
+        distances[i] = (d, i)
     distances.sort(key=operator.itemgetter(0))
 
     neighbors = [None]*k
@@ -36,21 +27,44 @@ def knn(k, training, test):
 
 def main():
     data = mat['X'].astype(np.float64)
-    labels = mat['Y'].astype(np.float64)
+    labels = mat['Y'].astype(int)
 
     total = len(data)
     split = 7000
-    k = 5
+    k = 1
 
     training = data[0:split]
     # testSet = data[split:total]
-    testSet = data[split:split+1]
+    testSet = data[split:total]
 
     # look at all the test points
-    for test in testSet:
+    correctcnt = 0
+    wrongcnt = 0
+    for i in range(len(testSet)):
+        test = testSet[i]
         neighbors = knn(k, training, test)
 
-        print neighbors
+        # print neighbors
         votes = [0]*10
+        for neighbor in neighbors:
+            digit = labels[neighbor]
+            votes[digit[0]] += 1
+        guess = 0
+        biggest = 0
+        for j in range(len(votes)):
+            if votes[j] > biggest:
+                guess = j
+                biggest = votes[j]
+
+        if guess == labels[i + split, 0]:
+            correctcnt += 1
+        else:
+            wrongcnt += 1
+
+        print "guess ", guess
+        print "actual ", labels[i + split, 0]
+
+        print correctcnt, wrongcnt
+
 
 main()
