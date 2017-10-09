5fc5ab60080c1fc203ada06fd294d3f546cf1eb8
larryzhao <larrylzhao@gmail.com>
2017-10-08 21:54:57 -0500

    5i done

--- hw_one/hw_one_5.py
+++ hw_one/hw_one_5.py
@@ -10,9 +10,18 @@ mat = scipy.io.loadmat('hw1data.mat')
 data = mat['X'].astype(int)
 labels = mat['Y'].astype(int)
 
+total = len(data)
+split = 7000
+k = 1
+
+trainingData = data[0:split]
+trainingLabels = labels[0:split]
+testSet = data[split:total]
+testLabels = labels[split:total]
+
 # calculate class priors
 classPriors = [0.0]*10
-for label in labels:
+for label in trainingLabels:
     classPriors[label[0]] += 1.0
 
 
@@ -22,47 +31,56 @@ mu = [0]*10
 for i in range(0, len(mu)):
     mu[i] = [0]*784
 
-for i in range(0, len(data)):
-    digit = labels[i][0]
+for i in range(0, len(trainingData)):
+    digit = trainingLabels[i][0]
     for column in range(0, 784):
-        mu[digit][column] = mu[digit][column] + data[i][column]/classPriors[digit]
+        mu[digit][column] = mu[digit][column] + trainingData[i][column]/classPriors[digit]
         # print column
 
 # sigma is a 10 x 784 x 784 matrix holding the covar matrices of each digit
 sigma = [0]*10
 
-for i in range(0, len(data)):
-    digit = labels[i][0]
-    xi = np.matrix(data[i]).astype(float)
+for i in range(0, len(trainingData)):
+    digit = trainingLabels[i][0]
+    xi = np.matrix(trainingData[i]).astype(float)
     mutemp = np.matrix(mu[digit]).astype(float)
     diff = (xi - mutemp)/classPriors[digit]
     diffT = diff.transpose()
     prod = diffT * diff
     sigma[digit] = sigma[digit] + prod
 
+correctcnt = 0
+wrongcnt = 0
 # calculate the probabilities and return the highest
-x = np.matrix(data[1]).astype(float)
-highestDigit = 0
-highestProb = 0.0
-for digit in range(0, 10):
-    mutemp = np.matrix(mu[digit]).astype(float)
-    diff = x - mutemp
-    diffT = diff.transpose()
-    sigtemp = sigma[digit]
-    siginv = np.linalg.inv(sigtemp + np.identity(784))
-    exp = -0.5 * (diff * siginv * diffT)[0,0]
+for i in range(len(testSet)):
+    x = np.matrix(testSet[i]).astype(float)
+    highestDigit = 0
+    highestProb = 99999999.9
+    for digit in range(0, 10):
+        mutemp = np.matrix(mu[digit]).astype(float)
+        diff = x - mutemp
+        diffT = diff.transpose()
+        sigtemp = sigma[digit]
+        siginv = np.linalg.inv(sigtemp + np.identity(784))
+        exp = -0.5 * (diff * siginv * diffT)[0,0]
 
-    #taking log to avoid overflow
-    (sign, logdet) = np.linalg.slogdet(sigtemp + np.identity(784))
-    logpi = 784 * 0.5 * np.log(2 * np.pi)
-    p = (exp * -1 * (logpi + (sign * logdet))) * classPriors[digit] / 10000.0
+        #taking log to avoid overflow
+        (sign, logdet) = np.linalg.slogdet(sigtemp + np.identity(784))
+        logpi = 784 * np.log(2 * np.pi)
+        p = -0.5 * (logpi + (sign * logdet) + exp) * classPriors[digit] / 10000.0
 
-    print exp, logdet, logpi, p
+        if p < highestProb:
+            highestProb = p
+            highestDigit = digit
 
-    if p > highestProb:
-        highestProb = p
-        highestDigit = digit
+        # print exp, logdet, logpi, p, highestProb, highestDigit
 
-print "actual: ", labels[1]
-print "guess: ", highestDigit
+    if highestDigit == testLabels[i, 0]:
+        correctcnt += 1
+    else:
+        wrongcnt += 1
 
+    print "guess ", highestDigit
+    print "actual ", testLabels[i, 0]
+
+    print correctcnt, wrongcnt
